29d9ac20440169aa1f83a1481416671906972f24
Segun Famisa <segunfamisa@gmail.com>
2016-08-09 19:59:41 +0100

    solution: Making Polygons
    https://www.hackerrank.com/contests/w22/challenges/polygon-making

---
+++ hackerrank/w22/making_polygons.py
@@ -0,0 +1,33 @@
+'''
+    Making Polygons
+
+    https://www.hackerrank.com/contests/w22/challenges/polygon-making
+'''
+def should_cut(sticks):
+    n = len(sticks)
+    s = sum(sticks)
+    for stick in sticks:
+        # check if any stick exists that is longer (or as long as)
+        # than the sum of the other sticks put together
+        if stick == 0 or stick >= s - stick:
+            return True
+    return False
+
+def Solution(n, a):
+    if n == 1:
+        # break 3 times to form a triangle
+        print 3
+    elif n > 1 and n < 3:
+        # essentially 2 sticks. Break one stick to get 3 sticks and then form a triangle
+        print 1
+    elif n >= 3:
+        # determine if you need to cut
+        if should_cut(a):
+            print 1
+        else:
+            print 0
+
+n = input()
+a = map(int, raw_input().split())
+
+Solution(n,a)
